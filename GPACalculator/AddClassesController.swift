af68bfca3e839200feac779ef89317e6a9a5f72e
AnimatorJoe <josephjin7@gmail.com>
2017-03-14 23:28:07 -0500

    Solved crashing

--- GPACalculator/AddClassesController.swift
+++ GPACalculator/AddClassesController.swift
@@ -12,11 +12,13 @@ import UIKit
 
 class AddClassesController: UIViewController {
     
+    @IBOutlet var warningLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        warningLabel.isHidden = true
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -25,15 +27,15 @@ class AddClassesController: UIViewController {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
