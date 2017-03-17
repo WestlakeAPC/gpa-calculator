087f3c9bbbf7efa3cb5d7bd8bf7b2e6380670b9f
Eli Joesph Bradley <elijos03@gmail.com>
2017-03-17 17:00:15 -0500

    Add 4.0 scale and letter grade.

--- GPACalculator/SecondViewController.swift
+++ GPACalculator/SecondViewController.swift
@@ -14,6 +14,7 @@ class SecondViewController: UIViewController {
     
     @IBOutlet weak var westlakeGPALabel: UILabel!
     @IBOutlet weak var standardGPALabel: UILabel!
+    @IBOutlet weak var letterGradeLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,14 +38,69 @@ class SecondViewController: UIViewController {
             
             westlakeGPALabel.text = ""
             standardGPALabel.text = ""
+            letterGradeLabel.text = ""
             return
         }
         
-        let westlakeGPA = totalWestlakeGrades / Double(Information.classesAndGrades.count)
+        let westlakeGPA = (totalWestlakeGrades / Double(Information.classesAndGrades.count)).rounded()
         westlakeGPALabel.text = "Westlake GPA: \(westlakeGPA)"
         
-        let standardGPA = totalStandardGrades / totalStandardCredit
-        standardGPALabel.text = "Standard GPA: \(standardGPA)"
+        let standardGPA = (totalStandardGrades / totalStandardCredit).rounded()
+        
+        var fourPointScale: Double = -1
+        var letter: String = ""
+        switch (standardGPA) {
+            case 0..<65:
+                fourPointScale = 0
+                letter = "E/F"
+            case 65...66:
+                fourPointScale = 1
+                letter = "D"
+            case 67...69:
+                fourPointScale = 1.3
+                letter = "D+"
+            case 70...72:
+                fourPointScale = 1.7
+                letter = "C-"
+            case 73...76:
+                fourPointScale = 2.0
+                letter = "C"
+            case 77...79:
+                fourPointScale = 2.3
+                letter = "C+"
+            case 80...82:
+                fourPointScale = 2.7
+                letter = "B-"
+            case 83...86:
+                fourPointScale = 3.0
+                letter = "B"
+            case 87...89:
+                fourPointScale = 3.3
+                letter = "B+"
+            case 90...92:
+                fourPointScale = 3.7
+                letter = "A-"
+            case 93...96:
+                fourPointScale = 4.0
+                letter = "A"
+            case 97...100:
+                fourPointScale = 4.0
+                letter = "A+"
+            default:
+                break
+        }
+        
+        guard fourPointScale != -1, !letter.isEmpty else {
+            print("Invalid grades")
+            
+            westlakeGPALabel.text = ""
+            standardGPALabel.text = ""
+            letterGradeLabel.text = ""
+            return
+        }
+        
+        standardGPALabel.text = "4.0 Scale GPA: \(fourPointScale)"
+        letterGradeLabel.text = "Letter Grade: \(letter)"
     }
 
     override func didReceiveMemoryWarning() {
