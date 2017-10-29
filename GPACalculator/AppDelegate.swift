1b6f098d403ea4017672b34e43b72d7f0f873564
AnimatorJoe <josephjin7@gmail.com>
2017-10-29 16:31:55 -0500

    Changing keyboard configs

--- GPACalculator/AppDelegate.swift
+++ GPACalculator/AppDelegate.swift
@@ -29,8 +29,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        IQKeyboardManager.sharedManager().previousNextDisplayMode = .alwaysHide
         IQKeyboardManager.sharedManager().enable = true
-     
+        
         UIApplication.shared.statusBarStyle = .lightContent
         
         return true
